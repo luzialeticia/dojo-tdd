4ce2b6846c789f4e2f7a0c2f0e76c441558f0c98
Marivone Vieira Pereira de Araujo <marivonearaujo@Marivones-MacBook-Pro.local>
2022-08-11 08:57:57 -0300

    diretório de Java calculator

---
+++ JAVA/CalculatorTDD/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.sum(2, 3));
+        System.out.println(calculator.subtract(2, 3));
+        System.out.println(calculator.multiplicate(2, 3));
+        System.out.println(calculator.divide(4, 2));
+
+    }
+}
---
+++ JAVA/CalculatorTDD/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+
+}
