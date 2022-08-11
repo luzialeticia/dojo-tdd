4a1fe96e290daf14083e9acf0ebfeae32a552c8a
Leticia Luzia <leticia.luzia@thoughtworks.com>
2022-08-11 12:49:07 -0300

    JS calculator solution

--- JS/Calculator/Calculator.js
+++ JS/Calculator/Calculator.js
@@ -1,6 +1,38 @@
 class Calculator {
-    static sum() {
-      //to-do
+    static sum(number1, number2) {
+      return number1 + number2;
+    }
+
+    static subtract(number1, number2) {
+      return number1 - number2;
+    }
+
+    static multiply(number1, number2) {
+      return number1 * number2;
+    }
+
+    static division(number1, number2) {
+      return number1 / number2;
+    }
+
+    static factorial(number) {
+      if (number === 0 || number === 1) {
+        return 0;
+      }
+  
+      let result = number;
+      for (let i = 1; i < number; i++) {
+        result *= number - i;
+      }
+  
+      return result;
+    }
+
+    static factorialOfTheSum(number1, number2) {
+      const sumValue = this.sum(number1, number2);
+      const result = this.factorial(sumValue);
+
+    return result;
     }
   }
   
--- JS/Calculator/Calculator.test.js
+++ JS/Calculator/Calculator.test.js
@@ -1,7 +1,42 @@
 const Calculator = require("./Calculator");
 
 describe("Calculator", () => {
+  const number1 = 3;
+  const number2 = 3;
+
   test("Should sum two values", () => {
-    // to-do
+    const result = Calculator.sum(number1, number2);
+
+    expect(result).toBe(6);
+  });
+
+  test("Should subtract two values", () => {
+    const result = Calculator.subtract(number1, number2);
+
+    expect(result).toBe(0);
+  });
+
+  test("Should multiply two values", () => {
+    const result = Calculator.multiply(number1, number2);
+
+    expect(result).toBe(9);
+  });
+
+  test("Should divide two numbers", () => {
+    const result = Calculator.division(number1, number2);
+
+    expect(result).toBe(1);
+  });
+
+  test("Should do the factorial", () => {
+    const result = Calculator.factorial(number1);
+
+    expect(result).toBe(6);
+  });
+
+  test("Should do the factorial between two values", () => {
+    const result = Calculator.factorialOfTheSum(number1, number2);
+
+    expect(result).toBe(720);
   });
 });
